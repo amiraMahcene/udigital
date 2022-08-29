fddd8d928e8a349c67bd074f3445e65378465789
Amira Mahcene <amiramahcene3@gmail.com>
2022-08-29 16:05:22 +0100

    I update the service page by using compenents

---
+++ src/compenents/services/serv.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './service.css';
+import elps1 from '../../images/EL3.svg'
+
+function Ser()
+{
+  return(
+    <div className="service">
+      <div className="left">
+        <img src={elps1} alt="" />
+      </div>
+      <div className="right">
+        <h1>hy</h1>
+        <p>bala bala bala bala </p>
+      </div>
+    </div>
+  )
+}
+
+export default Ser
